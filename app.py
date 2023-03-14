17e5b4537e11ba7db8b80b5f70cc8ffd4197bc9d
SUBASHUN VIKASH VIKASH <103167572+VIKASH1426@users.noreply.github.com>
2023-03-14 17:15:50 +0000

    dynamic datausing templates

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+JOBS=[
+  {
+    'id':1,
+  'title': 'data analyst'
+  'salary': 'Rs.1000000'
+  }
+  {
+    'id':2,
+  'title': 'data scientist'
+  'salary': 'Rs.1000040'
+  }
+{
+    'id':3,
+  'title': 'devop'
+  'salary': 'Rs.2000000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',job=JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
