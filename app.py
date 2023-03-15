87dc3136e316505c9145f94fb50e6748a01791e1
SUBASHUN VIKASH VIKASH <103167572+VIKASH1426@users.noreply.github.com>
2023-03-15 17:01:38 +0000

    ADDED DYNAMIC DATA

--- app.py
+++ app.py
@@ -5,24 +5,28 @@ app=Flask(__name__)
 JOBS=[
   {
     'id':1,
-  'title': 'data analyst'
-  'salary': 'Rs.1000000'
-  }
+  'title': 'data analyst',
+    'salary':'Rs.100000'
+  
+  },
   {
     'id':2,
-  'title': 'data scientist'
+  'title': 'data scientist',
   'salary': 'Rs.1000040'
-  }
+  },
 {
     'id':3,
-  'title': 'devop'
-  'salary': 'Rs.2000000'
+  'title': 'devop',
+  'salary':'Rs.2000000'
   }
 ]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html',job=JOBS)
+  return render_template('home.html',jobs=JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
